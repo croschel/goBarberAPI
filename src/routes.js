515f331d2325758cf69e05c729161a933f31b7c0
croschel <croschel000@gmail.com>
2020-02-04 19:38:19 -0300

    We can do Appointments now

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,30 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-
+// session
 routes.post('/session', SessionController.store);
 
+// users
+routes.post('/users', UserController.store);
+
 routes.use(authMiddleware); // Auth middleware
+
 routes.put('/users', UserController.update);
 
+// files
 routes.post('/files', upload.single('file'), FileController.store);
 
 // providers
 routes.get('/providers', ProviderController.index);
+
+// Appointments
+routes.post('/appointments', AppointmentController.store);
+
 export default routes;
