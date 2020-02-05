507582a5cfe70072b12699713a191222dde3b390
croschel <croschel000@gmail.com>
2020-02-04 21:38:16 -0300

    Provider can check them schedules

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
   async store(req, res) {
@@ -25,12 +27,59 @@ class AppointmentController {
       return res.status(401).json({ error: 'You need to choose a provider' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(401).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvalability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvalability) {
+      return res.status(401).json({ error: 'This date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
     return res.json(appointment);
+  }
+
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -26,8 +28,10 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 // providers
 routes.get('/providers', ProviderController.index);
+routes.get('/schedule', ScheduleController.index);
 
 // Appointments
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 export default routes;
